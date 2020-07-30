51c575e6f902d67dabea74a0f76587e4786835a8
Sone <sthaya@gmail.com>
2020-07-29 18:45:20 -0700

    created header component installed material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './component/Header'
 
 function App() {
   return (
-    <div className="App">
-     <h1>Youtube</h1>
+    <div className="app">
+      <h1>Youtube</h1>
+      <Header />
     </div>
   );
 }
