5fe337b7a284bae44d4a184c9900fc623ccd60c2
Sone <sthaya@gmail.com>
2020-07-29 19:18:19 -0700

    completed nav component and styled

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './component/Header'
 function App() {
   return (
     <div className="app">
-      <h1>Youtube</h1>
       <Header />
     </div>
   );
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import './Header.css'
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
 
 const Header = () => {
   return (
     <div className="header">
-      <h1>I am a header</h1>
+      
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+          alt="logo" />
+      </div>
+
+      <div className="header__input">
+        <input placeholder="Search" type="text" />
+        <SearchIcon className="header__inputButton" />
+      </div>
+      
+      <div className="header__icons">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon />
+        <Avatar />
+      </div>
+      
     </div>
   )
 }
