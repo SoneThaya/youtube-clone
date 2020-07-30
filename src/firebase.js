5201adbc164803db42853e04b7f5576d1e89789b
Sone <sthaya@gmail.com>
2020-07-29 18:26:50 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXW3rpYYutgcgEs-pX4oyOb3ihRDC08J0",
+  authDomain: "youtub-ced54.firebaseapp.com",
+  databaseURL: "https://youtub-ced54.firebaseio.com",
+  projectId: "youtub-ced54",
+  storageBucket: "youtub-ced54.appspot.com",
+  messagingSenderId: "1051197680404",
+  appId: "1:1051197680404:web:abfd54c7c7960265907001",
+  measurementId: "G-ZM5K5NQF8V"
+};
