0daab5399b2a7e1883ab3a53bf9cd193726a6c3d
Sone <sthaya@gmail.com>
2020-07-31 17:53:46 -0700

    created side bar row component added icons

--- src/component/RecommendedVideos.js
+++ src/component/RecommendedVideos.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './RecommendedVideos.css'
 
+
 const RecommendedVideos = () => {
   return (
     <div className="recommendedVideos">
       <h2>this is where videos go</h2>
+      
+      
     </div>
   )
 }
--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarRow from './SidebarRow'
+import WhatshotIcon from '@material-ui/icons/Whatshot'
+import HomeIcon from '@material-ui/icons/Home'
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h2>I am a sidebar</h2>
+      <SidebarRow Icon={HomeIcon} title='Home' />
+      <SidebarRow Icon={WhatshotIcon} title='Trending' />
+      <SidebarRow Icon={SubscriptionsIcon} title='Subscription' />
     </div>
   )
 }
