2998f203324f1c4222cf267ece1ce57187c78e02
Sone <sthaya@gmail.com>
2020-07-31 17:36:46 -0700

    created site bar and recommended videos component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './component/Header'
+import Sidebar from './component/Sidebar'
+import RecommendedVideos from './component/RecommendedVideos'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="app__page">
+        <Sidebar />
+        <RecommendedVideos />
+      </div>
     </div>
   );
 }
