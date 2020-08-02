40b876f3d89defb8b7d51418ae9ca68e1a000e4f
Sone <sthaya@gmail.com>
2020-08-01 21:30:53 -0700

    created channel row component and styled

--- src/component/SearchPage.js
+++ src/component/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './SearchPage.css'
 import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined'
+import ChannelRow from './ChannelRow'
 
 const SearchPage = () => {
   return (
@@ -11,7 +12,15 @@ const SearchPage = () => {
       </div>
 
       <hr />
-      
+
+      <ChannelRow
+        image="https://images.unsplash.com/photo-1596266953818-9ef30e4515ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80"
+        verified
+        channel="Clever Programmer"
+        subs="659K"
+        noOfVideos={382}
+        description="You can find awesome programming"
+      />
     </div>
   )
 }
