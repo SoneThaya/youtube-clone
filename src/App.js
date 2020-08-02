5190a8e7e658cf6860dbedcc08f25d820b704e28
Sone <sthaya@gmail.com>
2020-08-01 18:09:54 -0700

    created search page and styled

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import './App.css';
 import Header from './component/Header'
 import Sidebar from './component/Sidebar'
 import RecommendedVideos from './component/RecommendedVideos'
+import SearchPage from './component/SearchPage'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__page">
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/search/:searchTerm'>
+            <div className="app__page">
+              <Sidebar />
+              <SearchPage />
+            </div>
+          </Route>
+
+          <Route path='/'>
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
