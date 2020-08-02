31b3fa67142ae49a213c0388402085baebfad6f1
Sone <sthaya@gmail.com>
2020-08-01 17:34:27 -0700

    created video card component added dummy d

--- src/component/RecommendedVideos.js
+++ src/component/RecommendedVideos.js
@@ -1,12 +1,63 @@
 import React from 'react'
+import VideoCard from './VideoCard'
 import './RecommendedVideos.css'
 
 
 const RecommendedVideos = () => {
   return (
     <div className="recommendedVideos">
-      <h2>this is where videos go</h2>
-      
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Become a Web Developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://images.unsplash.com/photo-1593642532009-6ba71e22f468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          channel="Sonny Sangha"
+          image="https://images.unsplash.com/photo-1596178837086-25beb3638a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1337&q=80"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://images.unsplash.com/photo-1593642532009-6ba71e22f468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          channel="Sonny Sangha"
+          image="https://images.unsplash.com/photo-1596178837086-25beb3638a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1337&q=80"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://images.unsplash.com/photo-1593642532009-6ba71e22f468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          channel="Sonny Sangha"
+          image="https://images.unsplash.com/photo-1596178837086-25beb3638a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1337&q=80"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://images.unsplash.com/photo-1593642532009-6ba71e22f468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          channel="Sonny Sangha"
+          image="https://images.unsplash.com/photo-1596178837086-25beb3638a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1337&q=80"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://images.unsplash.com/photo-1593642532009-6ba71e22f468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          channel="Sonny Sangha"
+          image="https://images.unsplash.com/photo-1596178837086-25beb3638a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1337&q=80"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://images.unsplash.com/photo-1593642532009-6ba71e22f468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+          channel="Sonny Sangha"
+          image="https://images.unsplash.com/photo-1596178837086-25beb3638a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1337&q=80"
+        />
+       
+      </div>
       
     </div>
   )
