707b3ca702157a16db5ee08fc6fa52fa31b1e27f
Sone <sthaya@gmail.com>
2020-08-02 18:15:36 -0700

    created video row component and styled

--- src/component/SearchPage.js
+++ src/component/SearchPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './SearchPage.css'
 import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined'
 import ChannelRow from './ChannelRow'
+import VideoRow from './VideoRow'
 
 const SearchPage = () => {
   return (
@@ -20,6 +21,36 @@ const SearchPage = () => {
         subs="659K"
         noOfVideos={382}
         description="You can find awesome programming"
+      />
+
+      <hr />
+
+      <VideoRow
+        views="1.4M"
+        subs="659K"
+        description="Do you want a free one"
+        timestamp="59 seconds ago"
+        channel="Clever Programmer"
+        title="lets build"
+        image="https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+      />
+      <VideoRow
+        views="1.4M"
+        subs="659K"
+        description="Do you want a free one"
+        timestamp="59 seconds ago"
+        channel="Clever Programmer"
+        title="lets build"
+        image="https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+      />
+      <VideoRow
+        views="1.4M"
+        subs="659K"
+        description="Do you want a free one"
+        timestamp="59 seconds ago"
+        channel="Clever Programmer"
+        title="lets build"
+        image="https://images.unsplash.com/photo-1519741497674-611481863552?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
       />
     </div>
   )
